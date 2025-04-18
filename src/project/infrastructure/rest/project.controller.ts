d22e8a641284262b1268ab5ebe354359086a9aa2
thebladehit <boqdanyarmolka@gmail.com>
2025-04-18 22:31:13 +0300

    Update /projects controller routes

--- src/project/infrastructure/rest/project.controller.ts
+++ src/project/infrastructure/rest/project.controller.ts
@@ -2,7 +2,8 @@ import { Body, Controller, Param, Patch, Post } from '@nestjs/common';
 import { CreateProjectDto } from './dto/create-project.dto';
 import { CommandBus, QueryBus } from '@nestjs/cqrs';
 import { CreateProjectCommand } from '../../application/cqrs/commands/create-project.command';
-import { UpdateProjectCommand } from '../../application/cqrs/commands/update-project.command';
+import { UpdateProjectNameCommand } from '../../application/cqrs/commands/update-project-name.command';
+import { UpdateProjectRawDataCommand } from '../../application/cqrs/commands/update-project-rawData.command';
 
 @Controller('projects')
 export class ProjectController {
@@ -17,8 +18,13 @@ export class ProjectController {
     return this.commandBus.execute(new CreateProjectCommand(dto.name, dto.rawData));
   }
 
-  @Patch('/:id')
-  updateProject(@Param('id') id: string, @Body() dto: Partial<CreateProjectDto>) {
-    return this.commandBus.execute(new UpdateProjectCommand(id, dto.name, dto.rawData));
+  @Patch('/:id/name')
+  updateProjectName(@Param('id') id: string, @Body() dto: Partial<CreateProjectDto>) {
+    return this.commandBus.execute(new UpdateProjectNameCommand(id, dto.name!));
+  }
+
+  @Patch('/:id/rawData')
+  updateProjectRawData(@Param('id') id: string, @Body() dto: Partial<CreateProjectDto>) {
+    return this.commandBus.execute(new UpdateProjectRawDataCommand(id, dto.rawData!));
   }
 }
